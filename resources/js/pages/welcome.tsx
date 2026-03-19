58ab137db916d6bbc1654089e26a44e83a331956
Your Name <your_email@example.com>
2026-03-19 16:47:54 +0800

    finalize membership plan

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -5,6 +5,17 @@ import { Footer7 } from '@/components/footer7';
 import { ProductCard1 } from '@/components/product-card1';
 import  AnimatedContent from '@/components/AnimatedContent';
 import ScrollFloat from '@/components/ScrollFloat';
+import { BadgeCheck } from "lucide-react"
+import { Button  } from '@/components/ui/button';
+import ElectricBorder from '@/components/ElectricBorder';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 
 export default function Welcome({
@@ -39,18 +50,51 @@ export default function Welcome({
                             className="container mx-auto py-12"
                             direction="vertical"
                             distance={50}
-                            duration={0.8}
+                            duration={2}
                             ease="power2.out"
                         >
-                    <section className="container mx-auto">
-                        <h1 className='text-3xl font-bold mb-4'>Insurance Plans</h1>
-                        <div className="flex gap-6 overflow-x-auto pb-4">
+                    <section className="container flex flex-col item-center justify-center gap-8 lg:gap-15">
+                        <h1 className="text-3xl font-bold text-center relative inline-block">
+                            Membership Plans
+                            <span className="block h-1 w-100 bg-primary mx-auto mt-2"></span>
+                        </h1>
+                        <div className='flex w-full flex-col item-center justify-center gap-5 lg:flex-row overflow-x-auto'>
+                            {Array.from({length: 3}).map((_, i) => (
+                                <Card className='w-full max-w-sm gap-10'>
+                                    <CardHeader className='gap-4'>
+                                        <CardTitle className='text-center text-xl font-semibold tracking-tight'>Monthly</CardTitle>
+                                        <CardDescription className='text-center text-6xl font-bold'>$78</CardDescription>
+                                        <CardDescription className='text-center text-foreground/40'>per month</CardDescription>
+                                    </CardHeader>
+                                    <CardContent className=''>
+                                        <ul className='mt-1'>
+                                            <li className='mb-4 text-xs font-medium uppercase text-foreground/40'>Whats Included:</li>
+                                            {Array.from({length: 5}).map((_, i) => (
+                                                <li className='flex items-center gap-2 w-full justify-between font-medium mt-2'>
+                                                   <p>2,000 + production</p>
+                                                   <p className='text-foreground/40'> 2</p>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </CardContent>
+                                    <CardFooter>
+                                        <Button variant="default" size="lg" className='w-full cursor-pointer rounded-2xl'>
+                                            Start Monthly
+                                        </Button>
+                                    </CardFooter>
+                                </Card>
+                            ))}
+                        </div>
+                        {/* <div className="flex gap-6 overflow-x-auto pb-4">
                             {Array.from({ length: 8 }).map((_, i) => (
                                 <div key={i} className="min-w-xs">
                                     <ProductCard1 />
                                 </div>
                             ))}
-                        </div>
+                        </div> */}
+                    </section>
+                    <section className='w-full'>
+
                     </section>
                     </AnimatedContent>
                 </main>
